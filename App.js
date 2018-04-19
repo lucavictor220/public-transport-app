4256b9bfc0669f26127eb5b3544b95bb3fac1bc1
Vitiok <lucavictor220@gmail.com>
2018-04-19 12:37:36 +0300

    Added geolocation determination.

--- App.js
+++ App.js
@@ -9,32 +9,123 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
 } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { width, height } = Dimensions.get('window');
+
+const SCREEN_WIDTH = width;
+const SCREEN_HEIGHT = height;
+const ASPECT_RATIO = SCREEN_WIDTH / SCREEN_HEIGHT;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+
+const onCurrentPositionGeolocationFailure = (geo_error) => {
+  console.log('onCurrentPositionGeolocationFailure called');
+  alert(JSON.stringify(geo_error))
+};
+
+const onWatchPositionGeolocationFailure = (geo_error) => {
+  console.log('onWatchPositionGeolocationFailure called');
+  alert(JSON.stringify(geo_error))
+};
+
+const geolocationOptions = {
+  enableHighAccuracy: false,
+  timeout: 60000,
+};
+
+const watchPositionOptions = {
+  timeout: 60000,
+  enableHighAccuracy: false,
+  useSignificantChanges: false,
+};
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: {
+        latitude: 47.01667,
+        longitude: 28.83333,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      }
+    }
+  }
+
+  watchID: ? number = null;
+
+  createRegionObject = (position) => {
+    let lat = parseFloat(position.coords.latitude);
+    let long = parseFloat(position.coords.longitude);
+
+    return {
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    };
+  };
+
+  onCurrentLocationSuccess  = (position) => {
+    console.log('CALL onCurrentLocationSuccess');
+    const newPosition = this.createRegionObject(position);
+    console.log(newPosition);
+    this.setState((state) => {
+      return {
+        ...state,
+        position: { ...newPosition },
+        markerPosition: { latitude: newPosition.latitude, longitude: newPosition.longitude }
+      }
+    }, console.log(this.state));
+  };
+
+  onWatchPositionSuccess  = (position) => {
+    console.log('CALL onWatchPositionSuccess');
+    const newPosition = this.createRegionObject(position);
+    console.log(newPosition);
+    this.setState((state) => {
+      return {
+        ...state,
+        position: { ...newPosition },
+        markerPosition: { latitude: newPosition.latitude, longitude: newPosition.longitude }
+      }
+    }, console.log(this.state));
+  };
+
+  componentDidMount() {
+    console.log('COMPONENT MOUNTED');
+    navigator.geolocation.getCurrentPosition(this.onCurrentLocationSuccess, onCurrentPositionGeolocationFailure, geolocationOptions);
+    this.watchID = navigator.geolocation.watchPosition(this.onWatchPositionSuccess, onWatchPositionGeolocationFailure, watchPositionOptions);
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={{
-            latitude: 47.01667,
-            longitude: 28.83333,
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.09,
-          }}
-        />
+          initialRegion={this.state.position}
+          region={this.state.position}
+        >
+          <Marker
+            coordinate={this.state.markerPosition}
+          />
+        </MapView>
       </View>
     );
   }
@@ -48,10 +139,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   map: {
-    position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-  }
+    position: 'absolute',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
